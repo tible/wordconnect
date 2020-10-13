81612ce36159fac92b96eaa6e759081de6057718
tible <tiberiu.gociu@gmail.com>
2020-10-13 13:59:24 +0100

    adding some imports

--- wordconnect.py
+++ wordconnect.py
@@ -1,6 +1,13 @@
 # import
+from itertools import chain, permutations
+import enchant
 
 # def
+letters = []
+words = []
+numPermutations = 0
+dict = enchant.Dict('en_US')
+
 
 # main
-
+print("main!")
