8cbf56e537b701a344854b0f1b654cf8e0d63c37
tible <tiberiu.gociu@gmail.com>
2020-10-14 08:18:05 +0100

    Refactor Stage 1

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
 # This is a sample Python script.
+from itertools import chain, permutations
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+# define all lists
+letters = []
+words_created = []
+words_accepted = []
+dict_under_9 = []
+PermutationsNumber = 0
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# define the Permutation function
+def permutate(iterable_string):
+    iterable_list = list(iterable_string)
+    return chain.from_iterable(permutations(iterable_list, r) for r in range(len(iterable_list) + 1))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    with open('Collins Scrabble Words (2019).txt', 'r') as f_in:
+        dict_under_9 = f_in.read().splitlines()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    in_string = str(input("Insert letters here > "))
+    print(in_string)
+    print(list(in_string))
+    print("Processing...")
+    # print("dict_u9:", dict_under_9)
+    # words_created = list(permutate(in_string))
+    print("words_created =", words_created)
+    for elem in permutate(in_string):
+        PermutationsNumber += 1
+        word = ''.join(elem)
+        if word.upper() in dict_under_9 and len(word) > 2:
+            if word.upper() not in words_accepted:
+                words_accepted.append(word)
+
+    # removing duplicates because of string equality
+    words_accepted = list(dict.fromkeys(words_accepted))
+    words_accepted.sort()
+
+    for i in range(2, len(in_string)+1):
+        print("----------- {0} ----------".format(i))
+        temp_list = []
+        for wa in words_accepted:
+            if len(wa) == i:
+                # print("words_accepted:", words_accepted)
+                temp_list.append(wa)
+                # print(wa)
+        print(temp_list)
